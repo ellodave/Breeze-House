351de44858da17ecdf04d000a437b1af14ea1080
ellodave <34511073+ellodave@users.noreply.github.com>
2021-08-16 13:42:42 +0100

    Update homepage-redirect.php

--- experience-breeze-house/homepage-redirect.php
+++ experience-breeze-house/homepage-redirect.php
@@ -2,7 +2,7 @@ function redirect_expHomepage() {
     if( ! is_home() && ! is_front_page() )
         return;
 
-    wp_redirect( 'https://experience.breezehouse.co.uk/', 307 );
+    wp_redirect( 'https://experience.breezehouse.co.uk/', 301 );
     exit;
 }
 
