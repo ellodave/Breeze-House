ffaf1e39d730dab687f32f66b6c129c8bd520b32
ellodave <34511073+ellodave@users.noreply.github.com>
2021-08-16 13:37:05 +0100

    Homepage redirect script
    Redirect root homepage to experience subdomain.

--- experience-breeze-house/homepage-redirect.php
+++ experience-breeze-house/homepage-redirect.php
@@ -1,1 +1,9 @@
+function redirect_expHomepage() {
+    if( ! is_home() && ! is_front_page() )
+        return;
 
+    wp_redirect( 'https://experience.breezehouse.co.uk/', 307 );
+    exit;
+}
+
+add_action( 'template_redirect', 'redirect_expHomepage' );
