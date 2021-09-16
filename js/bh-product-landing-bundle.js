79c4d3b5c94b3e84d356860356d318eb930306d2
ellodave <34511073+ellodave@users.noreply.github.com>
2021-09-16 11:59:49 +0100

    Create bh-product-landing-bundle.js

---
+++ js/bh-product-landing-bundle.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper('.breeze-house-features', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // init: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
