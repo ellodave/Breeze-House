a77923e1dd4594e8926f92bfffacc17c4e571184
ellodave <34511073+ellodave@users.noreply.github.com>
2021-09-16 13:29:18 +0100

    Update collection-product-landing-bundle.js

--- js/collection-product-landing-bundle.js
+++ js/collection-product-landing-bundle.js
@@ -17,3 +17,22 @@ navigation: {
     prevEl: '.swiper-button-prev',
   },
 });
+var swiper = new Swiper('.collection-landing-bespoke-gallery', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // init: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
