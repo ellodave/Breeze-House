f8642d3de1d84cf58bf88896414d68a00ea230ea
ellodave <34511073+ellodave@users.noreply.github.com>
2021-10-11 16:20:50 +0100

    Update brochure-address-lookup.js

--- js/brochure-address-lookup.js
+++ js/brochure-address-lookup.js
@@ -1,5 +1,8 @@
 new clickToAddress({
     accessToken: '013c4-5976f-674e3-03a60',
+    showLogo: false,
+    defaultCountry: 'gbr',
+    getIpLocation: true,
     dom: {
         search:     'input_1_13',
         line_1:     'input_1_5_1',
