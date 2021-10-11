a80a3400b05817c74ad08aa592e5b7911d15acef
ellodave <34511073+ellodave@users.noreply.github.com>
2021-10-11 16:12:13 +0100

    Create brochure-address-lookup.js

---
+++ js/brochure-address-lookup.js
@@ -0,0 +1,10 @@
+new clickToAddress({
+    accessToken: '013c4-5976f-674e3-03a60',
+    dom: {
+        search:     'input_1_13',
+        line_1:     'input_1_5_1',
+        town:       'input_1_5_3',
+        postcode:   'input_1_5_5',
+    },
+    domMode: 'id'
+});
