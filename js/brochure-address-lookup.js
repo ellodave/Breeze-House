8ba17b82dc48f1330124f304b94d53af5ce9faa0
ellodave <34511073+ellodave@users.noreply.github.com>
2021-10-13 15:37:41 +0100

    Update brochure-address-lookup.js

--- js/brochure-address-lookup.js
+++ js/brochure-address-lookup.js
@@ -1,15 +1,15 @@
-jQuery(document).ready = function() {
-new clickToAddress({
-    accessToken: '013c4-5976f-674e3-03a60',
-    showLogo: false,
-    defaultCountry: 'gbr',
-    getIpLocation: true,
-    dom: {
-        search:     'input_1_13',
-        line_1:     'input_1_5_1',
-        town:       'input_1_5_3',
-        postcode:   'input_1_5_5',
-    },
-    domMode: 'id'
+jQuery(document).on('gform_page_loaded', function(event, form_id, current_page){
+               new clickToAddress({
+        accessToken: '013c4-5976f-674e3-03a60',
+			  showLogo: false,
+    		defaultCountry: 'gbr',
+   			getIpLocation: true,
+        dom: {
+            search:     'input_1_13',
+            town:       'input_1_5_3',
+            postcode:   'input_1_5_5',
+            line_1:     'input_1_5_1',
+        },
+        domMode: 'id'
+    });
 });
-};
