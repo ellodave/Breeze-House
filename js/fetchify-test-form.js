2c94396895a3bf36e999d98183303024f8f59964
ellodave <34511073+ellodave@users.noreply.github.com>
2021-10-13 13:35:59 +0100

    Update fetchify-test-form.js

--- js/fetchify-test-form.js
+++ js/fetchify-test-form.js
@@ -1,19 +1,3 @@
-jQuery(document).ready = function() {
-new clickToAddress({
-    accessToken: '013c4-5976f-674e3-03a60',
-    showLogo: false,
-    defaultCountry: 'gbr',
-    getIpLocation: true,
-    dom: {
-        search:     'input_5_2',
-        line_1:     'input_5_1_1',
-        town:       'input_5_1_3',
-        postcode:   'input_5_1_5',
-    },
-    domMode: 'id'
-});
-};
-
 new clickToAddress({
     accessToken: '013c4-5976f-674e3-03a60', // Replace this with your access token
     dom: {
