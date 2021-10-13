21fdf4d3877b0860e6998889205de62877d96a69
ellodave <34511073+ellodave@users.noreply.github.com>
2021-10-13 13:31:40 +0100

    Update fetchify-test-form.js

--- js/fetchify-test-form.js
+++ js/fetchify-test-form.js
@@ -13,3 +13,14 @@ new clickToAddress({
     domMode: 'id'
 });
 };
+
+new clickToAddress({
+    accessToken: '013c4-5976f-674e3-03a60', // Replace this with your access token
+    dom: {
+        search:     'input_5_2',
+        line_1:     'input_5_1_1',
+        town:       'input_5_1_3',
+        postcode:   'input_5_1_5'
+    },
+    domMode: 'id' // Use names to find form elements
+});
